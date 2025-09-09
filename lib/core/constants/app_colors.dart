6c6e7c01660a67e92120285fc494760eaac3fab2
njogubless <pnjogubless@gmail.com>
2025-09-09 11:21:36 +0300

    colors constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF4169E1);
+  static const Color primaryLight = Color(0xFF6B8AFF);
+  static const Color secondary = Color(0xFF00C896);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color surface = Colors.white;
+  static const Color error = Color(0xFFE53E3E);
+  static const Color textPrimary = Color(0xFF2D3748);
+  static const Color textSecondary = Color(0xFF718096);
+  static const Color borderColor = Color(0xFFE2E8F0);
+  static const Color success = Color(0xFF38A169);
+  static const Color splashBackground = Color(0xFF4169E1);
+}
